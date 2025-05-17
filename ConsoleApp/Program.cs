be09ec766a5539180fdff9a0c13d8147058949a4
ajosu <ajosu@DESKTOP-8A3KP44>
2025-05-17 11:33:18 -0600

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using Entities;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //instancia del gato
+
+        var myCat = new Cat() { Id = 1, Name = "Misingo" };
+        
+        Console.WriteLine("Id del gato" + myCat.Id);
+
+        Console.WriteLine("Nombre del gato" + myCat.Name);
+
+        Console.WriteLine("Ingrese una descripcion para el gato");
+
+        myCat.Description = Console.ReadLine();
+    }
+}
